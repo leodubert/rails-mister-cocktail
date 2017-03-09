0e40198c2bab45af31b14431ea5d56e837269f0f
Leo Dubert <leodubert@gmail.com>
2017-03-09 15:16:46 +0100

    created controller and models

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,16 @@
+class CocktailsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def add_dose
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/create'
+
+  get 'cocktails/add_dose'
+
+  get 'cocktails/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
